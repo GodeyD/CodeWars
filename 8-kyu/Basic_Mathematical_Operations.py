5bbc50bae61aa82ea98aa70d4b5e7af6153be155
Diego Ripari <diego_ripari@hotmail.com>
2024-05-14 18:33:28 +0300

    Update Basic_Mathematical_Operations.py

--- 8-kyu/Basic_Mathematical_Operations.py
+++ 8-kyu/Basic_Mathematical_Operations.py
@@ -11,4 +11,9 @@
 
 # My answer:
 def basic_op(operator, value1, value2):
-    #your code here
+    if operator == '+': return value1 + value2
+    if operator == '-': return value1 - value2
+    if operator == '*': return value1 * value2
+    if operator == '/': return value1 / value2
+
+print(basic_op('*', 5, 5))
