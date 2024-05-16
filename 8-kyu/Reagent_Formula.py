9bf39110d0ad2375103c828f5bc1c6e6c2f8fb96
Diego Ripari <diego_ripari@hotmail.com>
2024-05-16 19:26:44 +0300

    Update Reagent_Formula.py

--- 8-kyu/Reagent_Formula.py
+++ 8-kyu/Reagent_Formula.py
@@ -32,3 +32,10 @@
 # Happy Coding ^_^
 
 # My answer:
+def isValid(formula):
+    if (1 in formula and 2 in formula) or (3 in formula and 4 in formula) or (5 not in formula and 6 in formula) or (5 in formula and 6 not in formula) or (7 not in formula and 8 not in formula):
+        return False
+    else:
+        return True
+    
+print(isValid([1,3,7]))
