e7d1e993d21bb5b5dee3ed2df000d7341fe1a23d
Diego Ripari <diego_ripari@hotmail.com>
2024-05-22 22:27:03 +0300

    Points of reflection

--- 8-kyu/Sum The Strings.py
+++ 8-kyu/Sum The Strings.py
@@ -14,6 +14,19 @@
 # Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
 
 # My answer:
+a = input('Enter a number: ')
+b = input('number: ')
+
 def sum_str(a, b):
-    # happy coding !
-    pass
+    sum = 0
+    if int(a) == 0:
+        sum += int(a)
+    else:
+        sum += 0
+    if int(b) == 0:
+        sum += int(b)
+    else:
+        sum += 0   
+    return sum
+
+print(sum_str(a, b))
